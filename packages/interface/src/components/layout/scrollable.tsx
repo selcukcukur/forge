e126b4d954f0194d0e517a435d69a7508dd90038
selcukcukur <selcukcukur@outlook.com.tr>
2026-03-21 03:28:10 +0300

    feat(interface): add scrollable component

---
+++ packages/interface/src/components/layout/scrollable.tsx
@@ -0,0 +1,53 @@
+"use client"
+
+import type { ComponentProps } from "react"
+import { ScrollArea as Primitive } from "radix-ui"
+
+import { cn } from "@workspace/utility"
+
+export function Scrollable({
+  className,
+  children,
+  ...props
+}: ComponentProps<typeof Primitive.Root>) {
+  return (
+    <Primitive.Root
+      data-slot="scrollable"
+      className={cn("relative", className)}
+      {...props}
+    >
+      <Primitive.Viewport
+        data-slot="scrollable-viewport"
+        className="focus-visible:ring-ring/50 size-full rounded-[inherit] transition-[color,box-shadow] outline-none focus-visible:ring-[3px] focus-visible:outline-1"
+      >
+        {children}
+      </Primitive.Viewport>
+      <ScrollableBar />
+      <Primitive.Corner />
+    </Primitive.Root>
+  )
+}
+
+export function ScrollableBar({
+  className,
+  orientation = "vertical",
+  ...props
+}: ComponentProps<typeof Primitive.ScrollAreaScrollbar>) {
+  return (
+    <Primitive.ScrollAreaScrollbar
+      data-slot="scrollable-bar"
+      data-orientation={orientation}
+      orientation={orientation}
+      className={cn(
+        "data-horizontal:h-2.5 data-horizontal:flex-col data-horizontal:border-t data-horizontal:border-t-transparent data-vertical:h-full data-vertical:w-2.5 data-vertical:border-l data-vertical:border-l-transparent flex touch-none p-px transition-colors select-none",
+        className
+      )}
+      {...props}
+    >
+      <Primitive.ScrollAreaThumb
+        data-slot="scrollable-thumb"
+        className="rounded-full bg-border relative flex-1"
+      />
+    </Primitive.ScrollAreaScrollbar>
+  )
+}
--- packages/interface/src/index.tsx
+++ packages/interface/src/index.tsx
@@ -8,6 +8,7 @@ export * from "./providers/theme-provider"
 export { Collapsible, CollapsibleTrigger, CollapsibleContent } from "./components/layout/collapsible"
 export { Progressable, type ScrollProgressProps } from "./components/layout/progressable"
 export { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "./components/layout/resizable"
+export { Scrollable, ScrollableBar } from "./components/layout/scrollable"
 
 // General
 export * from "./components/client-only"
