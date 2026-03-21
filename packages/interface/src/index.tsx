59ade41ddd6577c2c347b1d7b70e40fd233bdb15
selcukcukur <selcukcukur@outlook.com.tr>
2026-03-21 03:26:53 +0300

    feat(interface): add resizable component

--- packages/interface/src/index.tsx
+++ packages/interface/src/index.tsx
@@ -7,6 +7,7 @@ export * from "./providers/theme-provider"
 // Layout
 export { Collapsible, CollapsibleTrigger, CollapsibleContent } from "./components/layout/collapsible"
 export { Progressable, type ScrollProgressProps } from "./components/layout/progressable"
+export { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "./components/layout/resizable"
 
 // General
 export * from "./components/client-only"
